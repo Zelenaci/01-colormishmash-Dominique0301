80c7265f7688ef76d937e533b381a03a18f22e0e
Dominik Slehofer <93545274+Dominique0301@users.noreply.github.com>
2022-01-10 14:17:23 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from os.path import basename, splitext
 import tkinter as tk
 from tkinter import Label, Button, Scale, Canvas
 
-from tkinter import HORIZONTAL, LEFT, Frame, Entry, Canvas, S, StringVar, IntVar, filedialog
+from tkinter import HORIZONTAL, LEFT, Frame, Entry, Canvas, S, StringVar, IntVar
 #dominik slehofer
 # from tkinter import ttk
-
 
 class Application(tk.Tk):
     name = basename(splitext(basename(__file__.capitalize()))[0])
@@ -17,6 +16,7 @@ class Application(tk.Tk):
         super().__init__(className=self.name)
         self.title(self.name)
         self.bind("<Escape>", self.quit)
+        self.protocol("WM_DELETE_WINDOW", self.quit)
 
 
 
@@ -104,17 +104,21 @@ class Application(tk.Tk):
         self.varG.trace("w", self.change)
         self.varB.trace("w", self.change)
 
+
+
+
         #malé okna
         self.frameMem=Frame(self)
         self.frameMem.pack()
+        self.canvasMem = []
         for row in range(3):
                 for column in range(7):
                         canvas = Canvas(self.frameMem, width=50, height=50, background="#000000")
                 
                         canvas.grid(row=row, column=column)
                         canvas.bind("<Button-1>", self.mousehandler)
-                        
-
+                        self.canvasMem.append(canvas)
+        self.colorLoad()
 
 
 
@@ -135,22 +139,60 @@ class Application(tk.Tk):
 
         self.canvasMain.config(background=color)#0 před 2 říká:vyplň prázdné místo nulami, 
         self.varMain.set(color)
-      
 
 
-    def quit(self, event=None):
-        super().quit()
 
 
     def mousehandler(self, event):
-        if self.cget("cursor") != "pencil":
+        if self.cget("cursor") != "pencil":#kliknu poprvé
                 self.config(cursor="pencil")
                 self.clr = event.widget.cget("background")
-        elif self.cget("cursor") == "pencil":
+        elif self.cget("cursor") == "pencil":#kliknu podruhé
                 self.config(cursor="")
                 event.widget.config(background=self.clr)
 
+                if event.widget is self.canvasMain:
+                        self.canvasColor2Slides(self.canvasMain)
 
+
+    def canvasColor2Slides(self, canvas):
+            colour = canvas.cget("background")
+            r = int(colour[1:3], 16)# 16 soustava
+            g = int(colour[3:5], 16)
+            b = int(colour[5:7], 16)
+            self.varR.set(r)
+            self.varG.set(g)
+            self.varB.set(b)
+
+    
+    def colorSave(self):
+            with open("color.txt", "w") as f:
+                    f.write(self.canvasMain.cget("background"+"\n"))
+                    for canvas in self.canvasMem:
+                            f.write(canvas.cget("background"+"\n"))
+
+
+    def colorLoad(self):
+        try:
+            with open("color.txt","r") as f:
+                    colorcode = f.readline().strip()# strip odstraní konec řádku
+                    self.canvasMain.config(background=colorcode)
+                    self.canvasColor2Slides(self.canvasMain)
+                    for canvas in self.canvasMem:
+                            colorcode = f.readline().strip()
+                            canvas.config(background=colorcode)
+        except FileNotFoundError:
+                print("Nepovedlo se načíst")
+
+
+    
+    
+    def quit(self, event=None):
+        print("Konec")
+        self.colorSave()
+        super().quit()
+
+        
 
 
 
